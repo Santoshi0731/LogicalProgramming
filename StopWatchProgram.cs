b0f73a00f8017389948f9117d88f33e560b0b41b
Santoshi0731 <118991307+Santoshi0731@users.noreply.github.com>
2023-01-31 14:28:04 +0530

    Add files via upload

---
+++ StopWatchProgram.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    public class StopWatchProgram
+    {
+        public static void DisplayStopwatchProgram()
+        {
+            Stopwatch stopwatch= new Stopwatch();
+            stopwatch.Start();
+
+            for(int i=0; i<100; i++) 
+            {
+                Thread.Sleep(2);
+            }
+            stopwatch.Stop();
+            Console.WriteLine("Time elapsed : {0}", stopwatch.Elapsed);
+        }
+    }
+}
