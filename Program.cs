630f4cec0191d102138afbcef7de230720a468bf
Santoshi0731 <118991307+Santoshi0731@users.noreply.github.com>
2023-01-31 14:20:57 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LogicalProgram
+{
+    public class Programs1
+    {
+        public static void Main(string[] args)
+        {
+
+            //StopWatchProgram.DisplayStopwatchProgram();
+            // PrimeNo.DisplayPrimeNo();
+            //PerfectNumber.FindNumber();
+            //FibonacciesSeries.FindTheFibonaccies();
+            CouponNumber.Coupon_Number();
+            // ReverseNumber.FindReverseNumber();
+        }
+    }
+}
+
